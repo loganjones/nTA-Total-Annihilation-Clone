4f40f101a4be46e2d8740e63faa3ff5eabb808b8
Logan Jones <lojones@fastmail.fm>
2016-05-02 18:03:43 -0700

    Added Apple platform includes and extra type definitions to be compatible with the existing code base.

--- nTA/Source/std_Platform.h
+++ nTA/Source/std_Platform.h
@@ -39,8 +39,76 @@
 
 		// Name given for the main window
 		char				MainWindowName[ 128 ];
-	};
+    };
 
+#define Param_t DWORD
+
+
+#elif __APPLE__
+#include "TargetConditionals.h"
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreGraphics/CoreGraphics.h>
+
+#define BOOL bool
+#define LPVOID void *
+#define LPCTSTR const char *
+#define LPTSTR char *
+#define LPSTR char *
+#define DWORD uint32_t
+#define WORD uint16_t
+#define BYTE uint8_t
+#define UINT32 uint32_t
+#define INT32 int32_t
+#define UINT16 uint16_t
+#define INT16 int16_t
+#define UINT8 uint8_t
+#define INT8 int8_t
+#define UINT unsigned int
+#define LRESULT long
+#define stricmp strcasecmp
+#define strnicmp strncasecmp
+#define ZeroMemory(P,SZ) memset(P,0,SZ)
+#define CALLBACK
+#define SOCKET int
+
+#define Param_t size_t
+
+#define LOWORD(l) ((WORD)(l))
+#define HIWORD(l) ((WORD)(((DWORD)(l) >> 16) & 0xFFFF))
+#define LOBYTE(w) ((BYTE)(w))
+#define HIBYTE(w) ((BYTE)(((WORD)(w) >> 8) & 0xFF))
+
+#define MAX_PATH 255
+
+typedef struct PALETTEENTRY {
+    BYTE peRed;
+    BYTE peGreen;
+    BYTE peBlue;
+    BYTE peFlags;
+} PALETTEENTRY, *LPPALETTEENTRY;
+
+#define INVALID_SOCKET (int)(~0)
+
+#if TARGET_IPHONE_SIMULATOR
+// iOS Simulator
+#elif TARGET_OS_IPHONE
+// iOS device
+#elif TARGET_OS_MAC
+// Other kinds of Mac OS
+#else
+#  error "Unknown Apple platform"
+#endif
+
+// Platform specific stuff used everywhere
+struct std_PlatformParameters
+{
+    // Name given for the main window
+    char				MainWindowName[ 128 ];
+};
+
+
+#else
+#  error "Unknown Platform"
 #endif
 
 
