c2f7315aeba41535238692f89b3cae1ece4787ec
Logan Jones <lojones@fastmail.fm>
2016-05-02 17:51:18 -0700

    Added Xcode workspace with OS X target application.

---
+++ nTA/OS X/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nTA
+//
+//  Created by Logan Jones on 1/17/16.
+//  Copyright © 2016 Logan Jones. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ nTA/OS X/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  nTA
+//
+//  Created by Logan Jones on 1/17/16.
+//  Copyright © 2016 Logan Jones. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
